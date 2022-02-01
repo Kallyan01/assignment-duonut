754047976938b4ebb83446e5d3e8e4c0549c645c
Kallyan01 <singhakallyan@gmail.com>
2022-02-01 16:24:39 +0530

    commit

---
+++ data.js
@@ -0,0 +1,30 @@
+let card_data=[
+    {
+        name: 'Mcq',
+        img_url: 'mcq.png'
+    },
+    {
+        name: 'Fill In The Blanks',
+        img_url: 'fib.png'
+    },
+    {
+        name: 'Code Editor',
+        img_url: 'demo.png'
+    },
+    {
+        name: 'Custom Layout',
+        img_url: 'demo.png'
+    },
+    {
+        name: 'Type Writer',
+        img_url: 'demo.png'
+    },
+    {
+        name: 'Analystics Layout',
+        img_url: 'demo.png'
+    },
+    {
+        name: 'Action',
+        img_url: 'demo.png'
+    },
+]
---
+++ script.js
@@ -0,0 +1,7 @@
+// let prev = document.getElementById('prev')
+function show(node) {
+    console.log(node.innerHTML)
+    var str = node.innerHTML;
+    prev.innerHTML= str
+    node.style='background:red;'
+}

70ce0f99dbef56afb8c459a4e5b906360ce16c81
Kallyan01 <singhakallyan@gmail.com>
2022-02-03 12:36:36 +0530

    added styling

--- data.js
+++ data.js
@@ -1,30 +1,42 @@
 let card_data=[
     {
         name: 'Mcq',
-        img_url: 'mcq.png'
+        img_url: 'mcq.png',
+        url: '/mcq'
     },
     {
         name: 'Fill In The Blanks',
-        img_url: 'fib.png'
+        img_url: 'fib.png',
+        url: '/fib'
     },
     {
         name: 'Code Editor',
-        img_url: 'demo.png'
+        img_url: 'demo.png',
+        url: '/ce'
     },
     {
         name: 'Custom Layout',
-        img_url: 'demo.png'
+        img_url: 'demo.png',
+        url: '/cl'
     },
     {
         name: 'Type Writer',
-        img_url: 'demo.png'
+        img_url: 'demo.png',
+        url: '/tw'
     },
     {
         name: 'Analystics Layout',
-        img_url: 'demo.png'
+        img_url: 'demo.png',
+        url: '/al'
     },
     {
         name: 'Action',
-        img_url: 'demo.png'
+        img_url: 'demo.png',
+        url: '/action'
+    },
+    {
+        name: 'Live',
+        img_url: 'demo.png',
+        url: '/live'
     },
 ]
--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 function show(node) {
     console.log(node.innerHTML)
     var str = node.innerHTML;
+    console.log(str)
+    console.log(prev)
     prev.innerHTML= str
-    node.style='background:red;'
+    // node.style='background:red;'
 }

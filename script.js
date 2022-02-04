5fc9ac47fdb249bbeaa3784b76b6135a2781fdea
Kallyan01 <singhakallyan@gmail.com>
2022-02-04 21:14:49 +0530

    added bag colors

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // let prev = document.getElementById('prev')
 function show(node) {
     let list_child = document.getElementsByClassName('card')
-    console.log(typeof (list_child))
+    // console.log(typeof (list_child))
     Array.from(list_child).forEach((node) => {
         node.classList.remove('active')
     })
-    console.log(node.innerHTML)
+    // console.log(node.innerHTML)
     var str = node.innerHTML;
     prev.innerHTML = str
     node.classList.add('active')
